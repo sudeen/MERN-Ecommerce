00be7edef9aa63fc3ffd1995f7de726709fdbd7e
sudeen <ranjitkarsudeen14@gmail.com>
2019-11-09 20:36:12 +1100

    authentication in ecommerce

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+require("dotenv").config();
+// import routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+
+// app
+const app = express();
+
+// db
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log("DB Connected"));
+
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+
+// routes middleware
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const { requireSignin, isAdmin, isAuth } = require("../controllers/auth");
+
+const { userById } = require("../controllers/user");
+
+router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
+    res.json({
+        user: req.profile
+    });
+});
+
+router.param("userId", userById);
+
+module.exports = router;
