b315df84031a764194b791adf311a0c81797499c
sudeen <ranjitkarsudeen14@gmail.com>
2019-11-10 21:28:46 +1100

    product read and delete successful

--- routes/product.js
+++ routes/product.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
-const {
-    create
-} = require("../controllers/product");
+const {create, productById, read, remove} = require("../controllers/product");
 
-
+router.get('/product/:productId', read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin,  create);
+router.delete('/product/:productId/:userId',requireSignin, isAuth, isAdmin, remove);
 
 router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
