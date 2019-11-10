aab49f37701a910799948e11d5a8a07f46c294a6
sudeen <ranjitkarsudeen14@gmail.com>
2019-11-10 23:09:42 +1100

    fetch products based on request query using postman

--- routes/product.js
+++ routes/product.js
@@ -4,12 +4,13 @@ const router = express.Router();
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
-const {create, productById, read, remove, update} = require("../controllers/product");
+const {create, productById, read, remove, update, list} = require("../controllers/product");
 
 router.get('/product/:productId', read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin,  create);
 router.delete('/product/:productId/:userId',requireSignin, isAuth, isAdmin, remove);
 router.put('/product/:productId/:userId',requireSignin, isAuth, isAdmin, update);
+router.get('/products', list);
 
 router.param("userId", userById);
 router.param("productId", productById);
