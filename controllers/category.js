b469424e543bb8d2dd8d0a0fbc4f7db4ff39049a
sudeen <ranjitkarsudeen14@gmail.com>
2019-11-10 22:03:04 +1100

    category read functionality

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,22 @@
 const Category = require("../models/category");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+exports.categoryById = (req, res, next, id) => {
+    Category.findById(id).exec((err, category) => {
+        if(err || !category){
+            return res.status(400).json({
+                error: "category does not exist"
+            });
+        }
+        req.category = category;
+        next();
+    });
+};
+
+exports.read = (req, res) => {
+    return res.json(req.category);
+};
+
 exports.create = (req, res) => {
     const category = new Category(req.body)
     category.save((err, data) => {
@@ -13,3 +29,4 @@ exports.create = (req, res) => {
     
 });
 };
+
--- routes/category.js
+++ routes/category.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
+
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
-const {
-    create
-} = require("../controllers/category");
+const {create, categoryById, read} = require("../controllers/category");
 
 
+router.get("/category/:categoryId", read);
 router.post("/category/create/:userId", requireSignin, isAuth, isAdmin,  create);
 
+
+router.param('categoryId', categoryById);
 router.param("userId", userById);
 
 module.exports = router;
