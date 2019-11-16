c369cb9880c95b0ea46786fe17ef8d23bbaa2873
sudeen <ranjitkarsudeen14@gmail.com>
2019-11-16 23:56:28 +1100

    search product API created for homepage

--- controllers/product.js
+++ controllers/product.js
@@ -241,3 +241,26 @@ exports.photo = (req, res, next) => {
     }
     next(); 
 };
+
+exports.listSearch = (req, res) => {
+    // create query object to hold search value and category value
+    const query = {}
+
+    // assign search value to query.name
+    if(req.query.search) {
+        query.name = {$regex: req.query.search, $options: 'i'}
+        // assign category value to query.category
+        if(req.query.category && req.query.category != 'All'){
+            query.category = req.query.category
+        }
+        // find the product based on query object with 2 properties
+        Product.find(query, (err, products) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(products)
+        }).select('-photo')
+    }
+}
--- routes/product.js
+++ routes/product.js
@@ -4,17 +4,20 @@ const router = express.Router();
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
-const {create, productById, read, remove, update, list, listRelated, listCategories, listBySearch, photo} = require("../controllers/product");
+const {create, productById, read, remove, update, list, listRelated, listCategories, listBySearch, photo, listSearch} = require("../controllers/product");
 
 router.get('/product/:productId', read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin,  create);
 router.delete('/product/:productId/:userId',requireSignin, isAuth, isAdmin, remove);
 router.put('/product/:productId/:userId',requireSignin, isAuth, isAdmin, update);
+
 router.get('/products', list);
+router.get('/products/search', listSearch);
 router.get('/products/related/:productId', listRelated);
 router.get('/products/categories', listCategories);
 router.post('/products/by/search', listBySearch);
 router.get('/product/photo/:productId', photo);
+
 
 router.param("userId", userById);
 router.param("productId", productById);
