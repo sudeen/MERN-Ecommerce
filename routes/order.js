cbe34528c46f3dd0a711a5096c08da157e23b062
sudeen <ranjitkarsudeen14@gmail.com>
2019-11-18 01:10:33 +1100

    quantity decrease and sold increment completed

--- routes/order.js
+++ routes/order.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const { requireSignin, isAuth } = require("../controllers/auth");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
 const { create } = require("../controllers/order");
+const { decreaseQuantity } = require("../controllers/product");
 
-router.post('/order/create/:userId', requireSignin, isAuth, addOrderToUserHistory, create);
+router.post('/order/create/:userId', requireSignin, 
+isAuth, 
+addOrderToUserHistory,
+decreaseQuantity, 
+create);
 
 router.param('userId', userById);
 
