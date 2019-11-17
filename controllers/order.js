ba2cf3ccc57a39ef435b9225b523085c8c112b53
sudeen <ranjitkarsudeen14@gmail.com>
2019-11-18 01:27:25 +1100

    listing orders in the admin dashboard

--- controllers/order.js
+++ controllers/order.js
@@ -1,16 +1,30 @@
-const { Order, CartItem } = require("../models/order");
-const { errorHandler } = require("../helpers/dbErrorHandler");
+const { Order, CartItem } = require('../models/order')
+const { errorHandler } = require('../helpers/dbErrorHandler')
 
 exports.create = (req, res) => {
-    // console.log("CREATE ORDER: ", req.body);
-    req.body.order.user = req.profile;
-    const order = new Order(req.body.order);
-    order.save((error, data) => {
-        if (error) {
-            return res.status(400).json({
-                error: errorHandler(error)
-            });
-        }
-        res.json(data);
-    });
-};
+  // console.log("CREATE ORDER: ", req.body)
+  req.body.order.user = req.profile
+  const order = new Order(req.body.order)
+  order.save((error, data) => {
+    if (error) {
+      return res.status(400).json({
+        error: errorHandler(error)
+      })
+    }
+    res.json(data)
+  })
+}
+
+exports.listOrders = (req, res) => {
+  Order.find()
+    .populate('user', '_id name address')
+    .sort('-created')
+    .exec((err, orders) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(error)
+        })
+      }
+      res.json(orders)
+    })
+}
--- routes/order.js
+++ routes/order.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { requireSignin, isAuth } = require("../controllers/auth");
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
-const { create } = require("../controllers/order");
+const { create, listOrders } = require("../controllers/order");
 const { decreaseQuantity } = require("../controllers/product");
 
 router.post('/order/create/:userId', requireSignin, 
@@ -10,6 +10,8 @@ isAuth,
 addOrderToUserHistory,
 decreaseQuantity, 
 create);
+
+router.get('/order/list/:userId', requireSignin, isAuth, isAdmin, listOrders );
 
 router.param('userId', userById);
 
