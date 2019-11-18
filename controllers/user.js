4c8ce6828444e4bf9716384547118e4ed7015101
sudeen <ranjitkarsudeen14@gmail.com>
2019-11-18 12:35:37 +1100

    password update fixed along with order status change

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,7 @@
 const User = require("../models/user");
+const { Order } = require("../models/order");
+const { errorHandler } = require("../helpers/dbErrorHandler");
+const _ = require('lodash');
 
 exports.userById = (req, res, next, id) => {
     User.findById(id).exec((err, user) => {
@@ -18,22 +21,83 @@ exports.read = (req, res) => {
     return res.json(req.profile);
 };
 
+// No password Update but name and email updates
+// exports.update = (req, res) => {
+//     User.findOneAndUpdate(
+//         { _id: req.profile._id },
+//         { $set: req.body },
+//         { new: true },
+//         (err, user) => {
+//             if (err) {
+//                 return res.status(400).json({
+//                     error: "You are not authorized to perform this action"
+//                 });
+//             }
+//             user.hashed_password = undefined;
+//             user.salt = undefined;
+//             res.json(user);
+//         }
+//     );
+// };
+
+// Working one but with error
+// exports.update = (req, res, next) => {
+//     let user = req.profile;
+//     user = _.extend(user, req.body); // extend - mutate the source object
+//     user.updated = Date.now();
+//     user.save(err => {
+//         if (err) {
+//             return res.status(400).json({
+//                 error: 'You are not authorized to perform this action'
+//             });
+//         }
+//         user.hashed_password = undefined;
+//         user.salt = undefined;
+//         res.json({ user });
+//     });
+// };
+
+
 exports.update = (req, res) => {
-    User.findOneAndUpdate(
-        { _id: req.profile._id },
-        { $set: req.body },
-        { new: true },
-        (err, user) => {
-            if (err) {
+    // console.log('UPDATE USER - req.user', req.user, 'UPDATE DATA', req.body);
+    const { name, password } = req.body;
+ 
+    User.findOne({ _id: req.profile._id }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User not found'
+            });
+        }
+        if (!name) {
+            return res.status(400).json({
+                error: 'Name is required'
+            });
+        } else {
+            user.name = name;
+        }
+ 
+        if (password) {
+            if (password.length < 6) {
                 return res.status(400).json({
-                    error: "You are not authorized to perform this action"
+                    error: 'Password should be min 6 characters long'
+                });
+            } else {
+                user.password = password;
+            }
+        }
+ 
+        user.save((err, updatedUser) => {
+            if (err) {
+                console.log('USER UPDATE ERROR', err);
+                return res.status(400).json({
+                    error: 'User update failed'
                 });
             }
-            user.hashed_password = undefined;
-            user.salt = undefined;
-            res.json(user);
-        }
-    );
+            updatedUser.hashed_password = undefined;
+            updatedUser.salt = undefined;
+            res.json(updatedUser);
+        });
+    });
 };
 
 exports.addOrderToUserHistory = (req, res, next) => {
@@ -64,4 +128,18 @@ exports.addOrderToUserHistory = (req, res, next) => {
             next();
         }
     );
+};
+
+exports.purchaseHistory = (req, res) => {
+    Order.find({ user: req.profile._id })
+        .populate("user", "_id name")
+        .sort("-created")
+        .exec((err, orders) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(orders);
+        });
 };
